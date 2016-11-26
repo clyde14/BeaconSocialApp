3ed96c3487bb86c935e7e094f7b65a1061848c6c
Chrissy Clyde <chrissyclyde@gmail.com>
2016-11-26 00:06:21 -0500

    Simple tests to ensure the right fields exist on the page to send a signal.

---
+++ features/sending_signal.feature
@@ -0,0 +1,13 @@
+# feature/creating_user.feature
+Feature: Create User
+As a registered user
+I want to send a signal
+So that I can share something with my followers
+
+Scenario: User can enter signal content
+When I go to the send signal page
+Then I should see a box to enter a signal's content
+
+Scenario: User sees submit button for signal
+When I go to the send signal page
+Then I should see a signal submit button
---
+++ features/step_definitions/send_signal_steps.rb
@@ -0,0 +1,18 @@
+#encoding: utf-8
+When(/^I go to the send signal page$/) do
+  visit "http://localhost:8000/signal_posts/new"
+  expect(page).to have_no_content 'Routing Error'
+end
+
+Then(/^I should see a box to enter a signal's (.*)$/) do |fieldname|
+  expect(page).to have_xpath '//textarea[@name="signal_post[%1$s]"]' % [fieldname]
+end
+
+Then(/^I should see a field to enter a signal's (.*)$/) do |fieldname|
+  expect(page).to have_xpath '//input[@name="signal_post[%1$s]"]' % [fieldname]
+end
+
+Then(/^I should see a signal submit button$/) do
+  expect(page).to have_no_content 'Submit button'
+  expect(page).to have_xpath '//form[@class="new_signal_post"]//input[@type="submit"]'
+end
