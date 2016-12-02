b04c6650c7ad07b02844c76c7f2fb20f37683c70
Chrissy Clyde <chrissyclyde@gmail.com>
2016-12-01 23:20:01 -0500

    Set up controller for sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :signal_posts
   resources :users
-  get 'welcome/index'
   
+  get 'welcome/index'
   root 'welcome#index'
   
   get '/signup', to: 'users#new'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
