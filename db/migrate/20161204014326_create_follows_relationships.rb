a35b50957f398c3a336610821456c880728f4104
Chrissy Clyde <chrissyclyde@gmail.com>
2016-12-03 20:53:43 -0500

    Create model for follows relationship.

---
+++ db/migrate/20161204014326_create_follows_relationships.rb
@@ -0,0 +1,13 @@
+class CreateFollowsRelationships < ActiveRecord::Migration[5.0]
+  def change
+    create_table :follows_relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+	add_index :follows_relationships, :follower_id
+    add_index :follows_relationships, :followed_id
+    add_index :follows_relationships, [:follower_id, :followed_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202033901) do
+ActiveRecord::Schema.define(version: 20161204014326) do
+
+  create_table "follows_relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_follows_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_relationships_on_follower_id"
+  end
 
   create_table "signal_posts", force: :cascade do |t|
     t.text     "content"
